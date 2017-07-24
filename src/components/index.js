8deba0bf70578f83c158e2a690e77676645ace4d
Jonathan Lie <jonathanlie94@gmail.com>
2017-07-24 14:53:19 +0800

    Remove unused import

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as Button } from './Button';
 export { default as InputText } from './InputText';
-export { default as ListItem } from './ListItem';
 export { default as MainContainer } from './MainContainer';
